8436b5e80728855edbfd98f726e5a3fa5f22050a
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2025-09-01 20:40:14 +0700

    add: new tf-lite model

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.unstable_enablePackageExports = false;
+defaultConfig.resolver.assetExts.push('tflite'); 
 
 module.exports = defaultConfig;
